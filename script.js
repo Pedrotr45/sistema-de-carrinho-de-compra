f4289465063eff1d3e8a3d67bc23e68243330046
Pedrotr45 <ph6878694@gmail.com>
2023-03-11 12:07:46 -0300

    sistema de carrinho de compra

---
+++ script.js
@@ -0,0 +1,88 @@
+const items = [
+    {
+      id:0,
+      nome: 'camiseta:',
+      imagem:"camiseta.png",
+      quantidade:0,
+    },
+
+    {
+      id:1,
+      nome: 'camiseta:',
+      imagem:"camiseta.png",
+      quantidade:0,
+    },
+    
+    {
+      id:2,
+      nome: 'camiseta:',
+      imagem:"camiseta.png",
+      quantidade:0,
+    },
+    
+
+
+
+   
+
+
+]
+
+
+
+inicializarLoja = () => {
+    var containerProdutos = document.getElementById('produtos');
+    items.map((val)=> {
+     
+       containerProdutos.innerHTML+= `
+       
+          <div class="produtos-single">
+            <img src="imagem/camiseta.png" alt="camiseta">
+              <p>`+val.nome+`</p>
+              <a key="`+val.id+`" href="#">Adicionar ao carrinho!</a>
+          </div>
+       
+       
+       `;
+       
+    })
+  
+  
+}
+
+inicializarLoja();
+
+ atualizarCarrinho = () => {
+  var containerCarrinho = document.getElementById('carrinho');
+   containerCarrinho.innerHTML="";
+   items.map((val)=> {
+      if(val.quantidade > 0) {
+       containerCarrinho.innerHTML+= `
+       
+       <p>`+val.nome+` | quantidade: `+val.quantidade+`</p>
+       <hr>
+       
+       `;
+      }
+    })
+
+   
+ 
+}
+
+
+
+   
+ 
+
+
+ var links = document.getElementsByTagName('a');
+
+ for(var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click",function() {
+        let key = this.getAttribute("key");
+        items[key].quantidade++;
+        atualizarCarrinho();
+        return false;
+    })
+ }
